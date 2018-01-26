c603c96d0ffd996432c16d3dd7f3080f484f9596
Ketrel <webmaster@krahs-emag.com>
2018-01-26 12:18:07 -0500

    A lot of python changes

--- python/launcher.py
+++ python/launcher.py
@@ -6,46 +6,60 @@ import socket
 import re
 import shlex
 
-def openConnection(mysock,size,regs):
-    conn, addr = mysock.accept()
-    conn.settimeout(30)
-    while True:
-        try:
-            data = conn.recv(size)
-        except socket.timeout:
-            print("Connection Closed Due To Timeout")
-            closeConnection(conn)
-            return None
-        if not data:
-            break
-        else:
-            matched = False
-            matchedKey = None
-            for key,reg in enumerate(regs):
-                match = reg.search(data.decode())
-                if match:
-                    matched = True
-                    matchedKey = key
-                    break;
-            if matched:
-                if matchedKey == 0:
-                    ret=raydioStart(match.group(1))
-                    print(ret)
-                    conn.send((ret+"\n").encode()[:1024])
-                elif matchedKey == 1:
-                    closeConnection(conn)
-                    return 'exit'
-                    break;
-            else:
-                print("Received: "+data.decode().rstrip())
-                conn.send(("Received: "+data.decode()).encode())
-    closeConnection(conn)
-    print("Connection Closed")
-    return None
+#from contextlib import contextmanager
+#@contextmanager
+#def activeConnection(connection):
+#    try:
+#        yield
+#    except socket.timeout:
+#        print("Connection Timed Out")
+#    finally:
+#        print("Connection Closed")
+#        connection.shutdown(socket.SHUT_RD)
+#        connection.close()
 
-def closeConnection(oldSock):
-    oldSock.shutdown(socket.SHUT_RD)
-    oldSock.close()
+class activeConnection():
+    def __init__(self, connection):
+        self.connection = connection
+    def __enter__(self):
+        return self
+    def __exit__(self, *args):
+        self.connection.shutdown(socket.SHUT_RD)
+        self.connection.close()
+        if args[0] == socket.timeout:
+            print("Connection Timed Out")
+            return True
+        else:
+            print("Connection Closed")
+
+def openConnection(mysock,size,regs):
+    conn, addr = (mysock.accept())
+    with activeConnection(conn):
+        conn.settimeout(30)
+        while True:
+            data = conn.recv(size)
+            if not data:
+                break
+            else:
+                matched = False
+                matchedKey = None
+                for key,reg in enumerate(regs):
+                    match = reg.search(data.decode())
+                    if match:
+                        matched = True
+                        matchedKey = key
+                        break;
+                if matched:
+                    if matchedKey == 0:
+                        ret=raydioStart(match.group(1))
+                        print(ret)
+                        conn.send((ret+"\n").encode()[:1024])
+                    elif matchedKey == 1:
+                        return 'exit'
+                else:
+                    print("Received: "+data.decode().rstrip())
+                    conn.send(("Received: "+data.decode()).encode())
+    return None
 
 def raydioStart(station):
     match = re.search(r"^[a-zA-Z0-9_-]+$",station)
@@ -69,20 +83,18 @@ def raydioStart(station):
 TCP_IP='0.0.0.0'
 TCP_PORT=8887
 BUFFER_SIZE=1024
-sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-sock.bind((TCP_IP,TCP_PORT))
-sock.listen(1)
+with socket.socket(socket.AF_INET,socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    sock.bind((TCP_IP,TCP_PORT))
+    sock.listen(1)
 
-#acks-42 = admin-command-kill-server-42
-regs=(
-        re.compile(r'^c: run ([a-zA-Z0-9_-]+)\n?$'),
-        re.compile(r'^acks-42\n?$'),
-     )
-
-while True:
-    ret = openConnection(sock,BUFFER_SIZE,regs)
-    if ret == 'exit':
-        break
-closeConnection(sock)
-print("Socket Closed")
+    #acks-42 = admin-command-kill-server-42
+    regs=(
+            re.compile(r'^c: run ([a-zA-Z0-9_-]+)\n?$'),
+            re.compile(r'^acks-42\n?$'),
+         )
+    while True:
+        ret = openConnection(sock,BUFFER_SIZE,regs)
+        if ret == 'exit':
+            break
+print("Sever Exited")
