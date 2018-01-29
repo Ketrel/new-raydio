63b4c9284736a949f93ea3bee41c3e671ffcdfe0
Ketrel <webmaster@krahs-emag.com>
2018-01-29 12:36:14 -0500

    Added start script and updated run command to reflect

---
+++ Dockerfiles/liquidSoapDeb/files/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+#Delete any existing unix sockets in the liquidsoap directory
+#
+
+#Ensure all required directories exist
+#
+
+#Start the launcher script
+"/usr/bin/python3" "/launcher.py"
--- run/run-liquidsoap.sh
+++ run/run-liquidsoap.sh
@@ -2,5 +2,6 @@
 if [ "${1}" = "int" ]; then
     docker run -it --rm -p 8887:8887 raydio-dev:liquidSoap /bin/bash
 else
-    docker run --rm -d -p 8887:8887 raydio-dev:liquidSoap /usr/bin/python3 /launcher.py
+#    docker run --rm -d -p 8887:8887 raydio-dev:liquidSoap /usr/bin/python3 /launcher.py
+    docker run --rm -d -p 8887:8887 raydio-dev:liquidSoap /start.sh
 fi
